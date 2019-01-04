051925f1b32a827b84a4c1d940ddf5bda8f75b15
kwhdyakdu <titanic41900@protonmail.com>
2019-01-04 11:04:34 +0100

    Handle keydown events

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 interface gridProps {
     grid: number[][]
 }
+
 function Grid(props: gridProps) {
     const { grid } = props
+
+    useEffect(() => {
+        document.addEventListener('keydown', keyDownHandler)
+        return () => {
+            document.removeEventListener('keydown', keyDownHandler)
+        }
+    })
+
+    const keyDownHandler = (e: KeyboardEvent) => {
+        const supportedKeys = [
+            'ArrowDown',
+            'ArrowUp',
+            'ArrowLeft',
+            'ArrowRight',
+        ]
+        const isSupported = supportedKeys.some(
+            supportedKey => supportedKey === e.code
+        )
+        if (isSupported) {
+            console.log(e.code)
+            e.preventDefault()
+        }
+    }
+
     return (
         <div>
-            {grid.map(line => (
-                <div>
-                    {line.map(cell => (
-                        <span>{cell}</span>
+            {grid.map((line, index) => (
+                <div key={'line' + index}>
+                    {line.map((cell, cellIndex) => (
+                        <span key={'line' + index + 'cell' + cellIndex}>
+                            {cell}
+                        </span>
                     ))}
                 </div>
             ))}
