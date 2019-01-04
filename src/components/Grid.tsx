d7648347f03b27a12e475b79814f1487bd53cf3d
kwhdyakdu <titanic41900@protonmail.com>
2019-01-04 10:43:04 +0100

    Add grid component

---
+++ src/components/Grid.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface gridProps {
+    grid: number[][];
+}
+function Grid(props:gridProps) {
+  const {grid} = props;
+  return (
+    <div>
+        {grid.map(line => <div>
+            {line.map(cell => <span>{cell}</span>)}
+        </div>)}
+    </div>
+  );
+}
+
+export default Grid;
--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React from "react";
 
 interface scoreProps {
     score: number;
